c0c0e85d3ebde5a7aeecea8d752e86aed9ec8ce4
Carolina Camblor <ccamblor@udesa.edu.ar>
2020-11-05 17:37:12 -0300

    tmdb listas home

---
+++ slider copy.js
@@ -0,0 +1,102 @@
+window.onload = function () {
+    var apiKey = "e57721559c7ea59e5e81582798c16c18"
+    var imgURL = "https://image.tmdb.org/t/p/w500/"
+    
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (let i = 0; i < data.results.length; i++) {
+            var pelicula = data.results[i];
+
+            console.log("holaa " + pelicula.original_title + " " + imgURL + pelicula.poster_path)
+            
+            var contenedorImagen = document.querySelector('#popular');
+
+            contenedorImagen.innerHTML +=
+            `
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img class="poster" src="${imgURL + pelicula.poster_path}" alt="${pelicula.original_title}">
+                    </div>
+                <div class="uk-card-body">
+                        <img class="estrellas" src="img/estrellas_home/estrellas5.png" alt="">
+                    </div>
+                </div>
+            </li>
+            `
+        };
+
+    })
+    .catch(function (error) {
+        console.log(`el error fue ${error}`)
+    })
+
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (let i = 0; i < data.results.length; i++) {
+            var pelicula = data.results[i];
+
+            var contenedorImagen = document.querySelector('#top');
+
+            contenedorImagen.innerHTML +=
+            `
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img class="poster" src="${imgURL + pelicula.poster_path}" alt="${pelicula.original_title}">
+                    </div>
+                <div class="uk-card-body">
+                        <img class="estrellas" src="img/estrellas_home/estrellas5.png" alt="">
+                    </div>
+                </div>
+            </li>
+            `
+        };
+        
+    })
+    .catch(function (error) {
+        console.log(`el error fue ${error}`)
+    })
+
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (let i = 0; i < data.results.length; i++) {
+            var pelicula = data.results[i];
+
+            var contenedorImagen = document.querySelector('#upcoming');
+
+            contenedorImagen.innerHTML +=
+            `
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img class="poster" src="${imgURL + pelicula.poster_path}" alt="${pelicula.original_title}">
+                    </div>
+                <div class="uk-card-body">
+                        <img class="estrellas" src="img/estrellas_home/estrellas5.png" alt="">
+                    </div>
+                </div>
+            </li>
+            `
+        };
+        
+    })
+    .catch(function (error) {
+        console.log(`el error fue ${error}`)
+    })
+}
